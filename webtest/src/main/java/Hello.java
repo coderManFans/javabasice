d6035d26fb239af8aad9fd7c587d51591fec9b00
coderManFans <2217011466@qq.com>
2016-11-14 16:12:47 +0800

    [INFO]:git测试 2016年11月14日16:12:42

---
+++ webtest/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ${fanchunshuai} on 2016-11-14.
+ *
+ * @version: V1.0
+ * @Desc:
+ * @Copyright (c) 2016 58到家-版权所有
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
