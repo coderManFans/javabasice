366bc2d3f3a7d388a3683c1ecfe15799ecf3fb2e
coderManFans <2217011466@qq.com>
2016-12-05 14:23:15 +0800

    [INFO]:git测试 2016年12月5日14:22:45

---
+++ elasticsearch-api/src/main/java/com/coderman/elasticsearchapi/model/UserTest.java
@@ -0,0 +1,11 @@
+package com.coderman.elasticsearchapi.model;
+
+/**
+ * Created by ${fanchunshuai} on 2016-12-5.
+ *
+ * @version: V1.0
+ * @Desc:
+ * @Copyright (c) 2016 58到家-版权所有
+ */
+public class UserTest {
+}
